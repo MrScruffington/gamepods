aadb090f478bfb0760e057cd616caa02da40501d
Scruffington <s.e.house@hotmail.co.uk>
2022-04-16 17:45:09 +0100

    Added debug flag to launch options

--- tf2/config/tf2_server.sh
+++ tf2/config/tf2_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./srcds_run -console -game tf -secured -port 27015 +sv_pure 1 +randommap +maxplayers 24
+./srcds_run -console -game tf -secured -port 27015 +sv_pure 1 +randommap +maxplayers 24 +sv_lan 0 -debug
 
 # For your server to be listed in the server browser you will need to add to the end of above: +sv_setsteamaccount [STEAMTOKEN]
 # Note: you will need to replace [STEAMTOKEN] with a token generated from here:
