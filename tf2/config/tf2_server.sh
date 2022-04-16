fbe22c7f357a6b813782c5ccbc2a18cdb9f5b41d
Scruffington <s.e.house@hotmail.co.uk>
2022-04-16 18:25:04 +0100

    Updated to include fully qualified path and added comment about commandline options

--- tf2/config/tf2_server.sh
+++ tf2/config/tf2_server.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
-./srcds_run -console -game tf -secured -port 27015 +sv_pure 1 +randommap +maxplayers 24 +sv_lan 0 -debug
-
-# For your server to be listed in the server browser you will need to add to the end of above: +sv_setsteamaccount [STEAMTOKEN]
+#!/bin/bash
+/srv/tf2/srcds_run -console -game tf -secured -port 27015 +sv_pure 1 +randommap +maxplayers 24 +sv_lan 0
+#
+# For your server to be listed in the server browser you will need to add: +sv_setsteamaccount [STEAMTOKEN]
 # Note: you will need to replace [STEAMTOKEN] with a token generated from here:
 # https://steamcommunity.com/dev/managegameservers
+#
+# Please follow this link for a full list of arguments you can pass to srcds_run
+# https://developer.valvesoftware.com/wiki/Command_Line_Options#Source_Dedicated_Server
