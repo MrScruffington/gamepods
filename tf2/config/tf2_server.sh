089b607af110ae8ecb070e496a46d53d54a29a98
Scruffington <s.e.house@hotmail.co.uk>
2022-04-16 18:28:03 +0100

    Included update command to run script

--- tf2/config/tf2_server.sh
+++ tf2/config/tf2_server.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Check for updates
+/srv/steamcmd/steamcmd.sh +force_install_dir /srv/tf2 +login anonymous +app_update 232250 +quit
+
+# Run server
 /srv/tf2/srcds_run -console -game tf -secured -port 27015 +sv_pure 1 +randommap +maxplayers 24 +sv_lan 0
-#
-# For your server to be listed in the server browser you will need to add: +sv_setsteamaccount [STEAMTOKEN]
+
+# For your server to be listed in the server browser you will need to append to line 6: +sv_setsteamaccount [STEAMTOKEN]
 # Note: you will need to replace [STEAMTOKEN] with a token generated from here:
 # https://steamcommunity.com/dev/managegameservers
 #
